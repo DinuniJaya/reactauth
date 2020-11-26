ca5ab89266f7d859961ed60c34c1e6f867b7dea3
DinuniJaya <dinunijaya@gmail.com>
2020-11-26 23:44:31 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';  
+import logo from './logo.svg';  
+import './App.css';  
+import Login from './Login';  
+import Register from './Register';  
+import Dashboard from './Dashboard';  
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';   
+function App() {  
+  return (  
+    <Router>    
+      <div className="container">    
+        <nav className="navbar navbar-expand-lg navheader">    
+          <div className="collapse navbar-collapse" >    
+            <ul className="navbar-nav mr-auto">    
+              <li className="nav-item">    
+                <Link to={'/Login'} className="nav-link">Login</Link>    
+              </li>    
+              <li className="nav-item">    
+                <Link to={'/Register'} className="nav-link">Sign Up</Link>    
+              </li>    
+               
+            </ul>    
+          </div>    
+        </nav> <br />    
+        <Switch>    
+          <Route exact path='/Login' component={Login} />    
+          <Route path='/Register' component={Register} />    
+       
+        </Switch>    
+        <Switch>  
+        <Route path='/Dashboard' component={Dashboard} />    
+        </Switch>  
+      </div>    
+    </Router>   
+  );  
+}  
+  
+export default App; 
